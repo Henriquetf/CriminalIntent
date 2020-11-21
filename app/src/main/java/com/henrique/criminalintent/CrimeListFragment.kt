848af169c7d5196ed6f14cc081b5d79616b4ca37
Henrique Torres <henriquetfw@gmail.com>
2020-11-20 22:30:04 -0300

    Add "New Crime" button on the toolbar of CrimeListFragment

--- app/src/main/java/com/henrique/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/henrique/criminalintent/CrimeListFragment.kt
@@ -3,9 +3,7 @@ package com.henrique.criminalintent
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -48,6 +46,12 @@ class CrimeListFragment : Fragment() {
         callbacks = null
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -75,6 +79,29 @@ class CrimeListFragment : Fragment() {
                 }
             }
         )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.fragment_crime_list, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.new_crime -> {
+                onClickNewCrime()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun onClickNewCrime() {
+        val crime = Crime()
+
+        crimeListViewModel.addCrime(crime)
+        callbacks?.onCrimeSelected(crime.id)
     }
 
     private inner class CrimeHolder(view: View) :
--- app/src/main/java/com/henrique/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/henrique/criminalintent/CrimeListViewModel.kt
@@ -6,4 +6,8 @@ class CrimeListViewModel : ViewModel() {
     private val crimeRepository = CrimeRepository.get()
 
     val crimeListLiveData = crimeRepository.getAllCrimes()
+
+    fun addCrime(crime: Crime) {
+        crimeRepository.insertCrime(crime)
+    }
 }
